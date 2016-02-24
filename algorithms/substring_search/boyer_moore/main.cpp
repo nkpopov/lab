652788735a31a269d706c2c6a062aa743795caba
Nikolay Popov <nkly.popov@gmail.com>
2016-02-24 22:51:32 +0300

    comments were added

--- algorithms/substring_search/boyer_moore/main.cpp
+++ algorithms/substring_search/boyer_moore/main.cpp
@@ -4,7 +4,29 @@
 #include <string>
 #include <algorithm>
 
+/* Compute shift table for Boyer-Moore algorithm. This version of
+ * algorithm is provided by Introduction To Algorithms video lectures
+ * by Robert Sedgewick from coursera and differs from wikipedia
+ * version of algorithm. 
+ * 
+ * Actual shift is computed in case of mismatch at the j'th character
+ * of pattern is computed using equation: shift = j - table[text[i + j]]. 
+ * This formula describes next sequence of rules:
+ * 
+ * (1) If mismatched character not in the pattern then we want to shift
+ *     to position right after it.
+ * (2) If mismatched character in the pattern then situation is more
+ *     complicated. In this case we want to align text with the same
+ *     right most character in the pattern, but we want to avoid
+ *     backup. That's why we are using std::max in the computation of
+ *     shift parameter.
+ */
 std::vector<int> compute_shift_table(const std::string &patt) {
+
+    /* First of all, set initial values for characters that are not in
+     * the pattern. Second step is to initialize values for characters
+     * that are in the pattern.
+     */   
     std::vector<int> right(256u, -1);
     for (int i = 0; i < patt.size(); ++i) {
         right[patt[i]] = i;
@@ -34,19 +56,18 @@ int main(int argc, char *argv[]) {
     
     int text_len = text.size();
     int patt_len = patt.size();
-    int skip     = 0;
+    int shift     = 0;
     
-    for (int i = 0; i < text_len - patt_len + 1; i += skip) {
-        skip = 0;
-
+    for (int i = 0; i < text_len - patt_len + 1; i += shift) {
+        shift = 0;
         for (int j = patt_len - 1; j >= 0; --j) {
             if (patt[j] != text[i + j]) {
-                skip = std::max(1, j - right[text[i + j]]);
+                shift = std::max(1, j - right[text[i + j]]);
                 break;
             }
         }
 
-        if (skip == 0) {
+        if (shift == 0) {
             printf("match is found at %d\n", i);
             printf("ihha! 1 2 3 4\n");
             return 0;
