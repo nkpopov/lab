50724d8e29b9ebd01a285fcb2db39bb48f67eb06
Nikolay Popov <nkly.popov@gmail.com>
2016-02-27 09:58:37 +0300

    additional methods for r-way trie were implemented

--- algorithms/tries/r-way/main.cpp
+++ algorithms/tries/r-way/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <utility>
 
 /* Node of r-way trie. Each node contains optional value and links to
  * next 256 nodes which correspond to all possible next characters in
@@ -56,6 +57,17 @@ class Trie {
          * Otherwise it will update value for provided key. 
          */
         void insert(const std::string &key, const T &value);
+
+        /* Get value that corresponds to key in arguments. There are
+         * different ways to design signature of this method. Most
+         * nice one with const T& return value. But this signature
+         * requires std::out_of_range exception thrown in case if no
+         * such key in the trie. 
+         */
+        std::pair<bool, T> get(const std::string &key) const;
+        
+        /* Check if key is in the trie */
+        bool contains(const std::string &key) const;
         
     private:
 
@@ -66,7 +78,10 @@ class Trie {
          * numeration starting from 0 for root and grows down.
          */
         Node<T> *insert_impl(Node<T> *node, const std::string &key,
-                const T &value, int depth); 
+                const T &value, int depth);
+
+        std::pair<bool, T> get_impl(Node<T> *node,
+                const std::string &key, int depth) const;
 
         /* Root node of trie */
         Node<T> *m_root;
@@ -87,6 +102,7 @@ void Trie<T>::insert(const std::string &key, const T &value) {
     m_root = insert_impl(m_root, key, value, 0);
 }
 
+
 template <typename T>
 Node<T> *Trie<T>::insert_impl(Node<T> *node, const std::string &key,
         const T &value, int depth) { 
@@ -96,18 +112,55 @@ Node<T> *Trie<T>::insert_impl(Node<T> *node, const std::string &key,
     }
 
     if (depth == key.size()) {
-        node.m_value = value;
+        node->m_value = value;
         return node;
     };
 
     char c = key[depth];
-    node.m_next[c] = insert_impl(node.m_next[c], key,
+    node->m_next[c] = insert_impl(node->m_next[c], key,
         value, depth + 1); 
     return node;
 }
 
+template <typename T>
+bool Trie<T>::contains(const std::string &key) const {
+    std::pair<bool, T> value = get(key);
+    return value.first;
+}
+
+template <typename T>
+std::pair<bool, T> Trie<T>::get(const std::string &key) const {
+    return get_impl(m_root, key, 0);
+}
+
+template <typename T>
+std::pair<bool, T> Trie<T>::get_impl(Node<T> *node,
+        const std::string &key, int depth) const {
+
+    if (node == NULL) {
+        return std::make_pair(false, T());
+    }
+
+    if (depth == key.size()) {
+        return std::make_pair(node->m_value != T(),
+            node->m_value);
+    }
+
+    return get_impl(node->m_next[key[depth]], key,
+            depth + 1);
+}
+
 int main(int argc, char *argv[]) {
+
     Trie<int> trie;
+    trie.insert("potato",   1);
+    trie.insert("pomidoro", 2);
+    trie.insert("carrot",   3);
+
+    printf("potato:   %d\n", trie.get("potato").second);
+    printf("pomidoro: %d\n", trie.get("pomidoro").second);
+    printf("carrot:   %d\n", trie.get("carrot").second);
+    printf("tomato:   %d\n", trie.get("tomato").second);
     return 0;
 }
     
